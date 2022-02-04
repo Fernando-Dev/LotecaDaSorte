d05d12e2fd6e5a0c2ab564e88706be0aa717e497
Fernando Nascimento <fernando_dev@outlook.com.br>
2022-02-03 21:20:06 -0300

    alteracao no loop do carrosel

--- app/src/main/java/br/fernando/lotecadasorte/ui/view/main/MainActivity.kt
+++ app/src/main/java/br/fernando/lotecadasorte/ui/view/main/MainActivity.kt
@@ -1,12 +1,11 @@
 package br.fernando.lotecadasorte.ui.view.main
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.widget.LinearLayout
-import androidx.core.view.postDelayed
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,14 +20,17 @@ import br.fernando.lotecadasorte.ui.view.main.adapter.OnItemClickListener
 import br.fernando.lotecadasorte.ui.view.resultado.LoteriaResultadoActivity
 import br.fernando.lotecadasorte.viewmodel.main.MainViewModel
 import com.google.android.material.button.MaterialButton
+import java.util.*
 
-//import com.synnapps.carouselview.CarouselView
 
 class MainActivity : AppCompatActivity(), OnItemClickListener {
 
     private lateinit var mMainViewModel: MainViewModel
     private lateinit var mRecyclerViewMenuLoterias: RecyclerView
     private lateinit var mRecyclerViewProximosSorteios: RecyclerView
+    private var duration: Long = 5000
+    private var pixelsToMove: Int = 500
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +38,6 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
 
         mMainViewModel = ViewModelProvider.NewInstanceFactory().create(MainViewModel::class.java)
 
-//        val carouselProximosSorteios = findViewById<CarouselView>(R.id.carousel_proximos_sorteios)
         mRecyclerViewProximosSorteios = findViewById(R.id.main_recycler_proximos_sorteios)
         mRecyclerViewMenuLoterias = findViewById(R.id.main_recycler_menu_loterias)
         val btnAumenteSuaChance = findViewById<MaterialButton>(R.id.btn_listar_loterias)
@@ -58,16 +59,50 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
                 adapter = MainActivityProximoSorteioAdapter(list)
                 val snapHelper = LinearSnapHelper()
                 snapHelper.attachToRecyclerView(mRecyclerViewProximosSorteios)
-                postDelayed(Runnable {
-                    smoothScrollBy(400,0)
-                },2000)
+                val mHandler: Handler = Handler(Looper.getMainLooper())
+                val SCROLLING_RUNNABLE = object : Runnable {
+                    override fun run() {
+                        mRecyclerViewProximosSorteios.smoothScrollBy(pixelsToMove, 0)
+                        mHandler.postDelayed(this, duration)
+                    }
+                }
+                addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+
+                        if (dx > 0) {
+                            Log.i(localClassName, "valor do deslocamento" + dx.toString())
+                            if (isLastVisable(MainActivityProximoSorteioAdapter(list))) {
+                                mHandler.removeCallbacks(SCROLLING_RUNNABLE)
+                                val postHandler = Handler(Looper.myLooper()!!)
+                                postHandler.postDelayed(object : Runnable {
+                                    override fun run() {
+                                        adapter = null
+                                        adapter = MainActivityProximoSorteioAdapter(list)
+                                        mHandler.postDelayed(SCROLLING_RUNNABLE, 2000)
+                                    }
+                                }, 2000)
+                            }
+                        }
+
+                    }
+                })
+                mHandler.postDelayed(SCROLLING_RUNNABLE, 2000)
             }
+
             mRecyclerViewMenuLoterias.apply {
                 hasFixedSize()
                 isNestedScrollingEnabled = false
                 adapter = MainActivityMenuAdapter(list, this@MainActivity)
             }
         })
+    }
+
+    private fun isLastVisable(adapter: MainActivityProximoSorteioAdapter): Boolean {
+        val layoutManager = mRecyclerViewProximosSorteios.layoutManager as LinearLayoutManager
+        val pos = layoutManager.findLastVisibleItemPosition()
+        val numItems = adapter.itemCount
+        return (pos >= numItems - 1)
     }
 
     override fun onItemClick(id: Int, nomeLoteria: String) {
