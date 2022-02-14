4d4ad8f8685f19ab049a7b48ac5014f48158062b
Fernando Nascimento <fernando_dev@outlook.com.br>
2022-02-13 22:51:19 -0300

    alterando carousel

--- app/src/main/java/br/fernando/lotecadasorte/ui/view/main/MainActivity.kt
+++ app/src/main/java/br/fernando/lotecadasorte/ui/view/main/MainActivity.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import br.fernando.lotecadasorte.R
 import br.fernando.lotecadasorte.model.*
 import br.fernando.lotecadasorte.ui.view.listaloterias.LoteriasLista
@@ -20,6 +20,8 @@ import br.fernando.lotecadasorte.ui.view.main.adapter.OnItemClickListener
 import br.fernando.lotecadasorte.ui.view.resultado.LoteriaResultadoActivity
 import br.fernando.lotecadasorte.viewmodel.main.MainViewModel
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import java.util.*
 
 
@@ -27,9 +29,10 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
 
     private lateinit var mMainViewModel: MainViewModel
     private lateinit var mRecyclerViewMenuLoterias: RecyclerView
-    private lateinit var mRecyclerViewProximosSorteios: RecyclerView
-    private var duration: Long = 1000
-    private var pixelsToMove: Int = 500
+    private lateinit var mViewPagerProximosSorteios: ViewPager2
+    private lateinit var mTabLayoutProximosSorteios: TabLayout
+    private lateinit var mViewPagerHandler: Handler
+    private lateinit var mViewPagerRun: Runnable
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +41,8 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
 
         mMainViewModel = ViewModelProvider.NewInstanceFactory().create(MainViewModel::class.java)
 
-        mRecyclerViewProximosSorteios = findViewById(R.id.main_recycler_proximos_sorteios)
+        mViewPagerProximosSorteios = findViewById(R.id.main_view_pager_proximos_sorteios)
+        mTabLayoutProximosSorteios = findViewById(R.id.main_tab_proximos_sorteios);
         mRecyclerViewMenuLoterias = findViewById(R.id.main_recycler_menu_loterias)
         val btnAumenteSuaChance = findViewById<MaterialButton>(R.id.btn_listar_loterias)
 
@@ -46,19 +50,43 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
             startActivity(Intent(this, LoteriasLista::class.java))
         }
 
-        configurarMenuPrincipal()
+        configurarTelaPrincipal()
 
     }
 
-    private fun configurarMenuPrincipal() {
+    private fun configurarTelaPrincipal() {
         mMainViewModel.retornaTodasLoterias()
         mMainViewModel.listaLoterias.observe(this, Observer { list ->
 
-            mRecyclerViewProximosSorteios.apply {
-                hasFixedSize()
+            mViewPagerProximosSorteios.apply {
                 adapter = MainActivityProximoSorteioAdapter(list)
-                val snapHelper = LinearSnapHelper()
-                snapHelper.attachToRecyclerView(mRecyclerViewProximosSorteios)
+                offscreenPageLimit = 1
+                clipToPadding = false
+                clipChildren = false
+                getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+                TabLayoutMediator(
+                    mTabLayoutProximosSorteios,
+                    mViewPagerProximosSorteios
+                ) { tab, position -> }.attach()
+
+                mViewPagerHandler = Handler(Looper.getMainLooper())
+                mViewPagerRun = Runnable {
+                    mViewPagerProximosSorteios.currentItem =
+                        mViewPagerProximosSorteios.currentItem + 1
+
+                }
+                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                    override fun onPageSelected(position: Int) {
+                        super.onPageSelected(position)
+                        if (position == adapter?.itemCount) {
+                            mViewPagerHandler.removeCallbacks(mViewPagerRun)
+                            adapter = null
+                            adapter = MainActivityProximoSorteioAdapter(list)
+                        }
+                        mViewPagerHandler.postDelayed(mViewPagerRun, 3000)
+                    }
+                })
+
             }
 
             mRecyclerViewMenuLoterias.apply {
@@ -79,6 +107,16 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
 
     override fun onBackPressed() {
         finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mViewPagerHandler.removeCallbacks(mViewPagerRun)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mViewPagerHandler.postDelayed(mViewPagerRun, 3000)
     }
 
 
--- app/src/main/java/br/fernando/lotecadasorte/ui/view/main/adapter/MainActivityProximoSorteioAdapter.kt
+++ app/src/main/java/br/fernando/lotecadasorte/ui/view/main/adapter/MainActivityProximoSorteioAdapter.kt
@@ -3,11 +3,16 @@ package br.fernando.lotecadasorte.ui.view.main.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import br.fernando.lotecadasorte.R
 import br.fernando.lotecadasorte.model.Loteria
 
-class MainActivityProximoSorteioAdapter(private val listaLoteria: ArrayList<Loteria>) :
+class MainActivityProximoSorteioAdapter(
+    /*val viewPager: ViewPager2,*/
+    private val listaLoteria: ArrayList<Loteria>
+) :
     RecyclerView.Adapter<MainActivityProximoSorteioViewHolder>() {
+
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -28,8 +33,20 @@ class MainActivityProximoSorteioAdapter(private val listaLoteria: ArrayList<Lote
         holder.valorAcumulado.text = listaLoteria[position].acumuladaProximoSorteio
         holder.proximoConcurso.text = listaLoteria[position].proximoSorteio.toString()
         holder.dataProximoConcurso.text = listaLoteria[position].dataProximoSorteio
+
+        /*if (position == listaLoteria.size - 2) {
+            viewPager.post(run)
+        }*/
     }
 
     override fun getItemCount(): Int = listaLoteria.size
+
+    private val run = Runnable {
+        val listAux = ArrayList<Loteria>()
+        listAux.addAll(listaLoteria)
+        listaLoteria.clear()
+        listaLoteria.addAll(listAux)
+        notifyDataSetChanged()
+    }
 
 }
